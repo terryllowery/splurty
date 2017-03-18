23394327cd4741b7eaa3c35436702708778fc5a0
Terry Lowery <terry@lowerytech.net>
2017-03-18 04:40:24 +0000

    Completed about page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -11,10 +11,11 @@ class QuotesController < ApplicationController
       flash[:error] = '<strong>Could not save,</strong> the data you entered is invalid.'
     end
     redirect_to root_path
-
   end
   private
   def quote_params
     params.require(:quote).permit(:saying, :author)
+  end
+  def about
   end
 end
